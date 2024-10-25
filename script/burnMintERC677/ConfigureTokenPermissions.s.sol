2b9112961e6bd258763b565019a5990dd9178488
cromewar <vasiliy@cromewar.com>
2024-10-24 19:17:17 -0500

    feature: adding some custom scripts for token configuration and deployment

---
+++ script/burnMintERC677/ConfigureTokenPermissions.s.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {Script} from "forge-std/Script.sol";
+import {CCIPLocalSimulatorFork, Register} from "@chainlink/local/src/ccip/CCIPLocalSimulatorFork.sol";
+import {BurnMintTokenPool} from "@chainlink/contracts-ccip/src/v0.8/ccip/pools/BurnMintTokenPool.sol";
+import {IBurnMintERC20} from "@chainlink/contracts-ccip/src/v0.8/shared/token/ERC20/IBurnMintERC20.sol";
+import {RegistryModuleOwnerCustom} from "@chainlink/contracts-ccip/src/v0.8/ccip/tokenAdminRegistry/RegistryModuleOwnerCustom.sol";
+import {TokenAdminRegistry} from "@chainlink/contracts-ccip/src/v0.8/ccip/tokenAdminRegistry/TokenAdminRegistry.sol";
+import {IBurnMintERC677} from "../../src/interface/IBurnMintERC677.sol";
+
+contract DeployTokenAndPoolScript is Script {
+    RegistryModuleOwnerCustom registryModuleOwnerCustom;
+    TokenAdminRegistry tokenAdminRegistry;
+    CCIPLocalSimulatorFork public ccipLocalSimulatorFork;
+    Register.NetworkDetails networkDetails;
+
+    function setUp() public {
+        ccipLocalSimulatorFork = new CCIPLocalSimulatorFork();
+        networkDetails = ccipLocalSimulatorFork.getNetworkDetails(
+            block.chainid
+        );
+
+        registryModuleOwnerCustom = RegistryModuleOwnerCustom(
+            networkDetails.registryModuleOwnerCustomAddress
+        );
+        tokenAdminRegistry = TokenAdminRegistry(
+            networkDetails.tokenAdminRegistryAddress
+        );
+    }
+
+    function run(address tokenAddress, address poolAddress) public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        IBurnMintERC677 myToken = IBurnMintERC677(address(tokenAddress));
+
+        // Step 3) Grant Mint and Burn roles to BurnMintTokenPool
+        myToken.grantMintAndBurnRoles(poolAddress);
+
+        // Step 4) Claim Admin role
+        registryModuleOwnerCustom.registerAdminViaOwner(address(myToken));
+
+        // // Step 5) Accept Admin role
+        tokenAdminRegistry.acceptAdminRole(address(myToken));
+
+        // // Step 6) Link token to pool
+        tokenAdminRegistry.setPool(address(myToken), poolAddress);
+
+        vm.stopBroadcast();
+    }
+}
